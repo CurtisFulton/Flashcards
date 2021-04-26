f52bf2dbc78b1ef6f39f0dfc9f41d34140bfafa8
Curtis Fulton <Curtis.R.Fulton@Gmail.com>
2021-04-26 14:36:54 +1000

    Initial project setup

---
+++ Flashcards.Mobile/Flashcards.Mobile/AppShell.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.CommunityToolkit.ObjectModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Flashcards.Mobile
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AppShell : Shell
+    {
+        public ICommand SignOutCommand { get; }
+
+        public AppShell()
+        {
+            this.SignOutCommand = new AsyncCommand(this.SignOut);
+            this.BindingContext = this;
+            InitializeComponent();
+        }
+
+        private async Task SignOut()
+        {
+            await this.GoToAsync("//login");
+        }
+    }
+}
---
+++ Flashcards.Mobile/Flashcards.Mobile/Pages/LoginPageViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.CommunityToolkit.ObjectModel;
+using Xamarin.Forms;
+
+namespace Flashcards.Mobile.Pages
+{
+    public class LoginPageViewModel
+    {
+        public ICommand LoginCommand { get; private set; }
+
+        public LoginPageViewModel()
+        {
+            this.LoginCommand = new AsyncCommand(this.LogUserIn);
+        }
+
+        private async Task LogUserIn()
+        {
+            await Shell.Current.GoToAsync("//study");
+        }
+    }
+}
